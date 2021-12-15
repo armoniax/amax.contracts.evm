5a576996528c76b36fe4f060b4ccbe2b605dc5e6
SeanChen <aws4amazon@163.com>
2021-12-15 16:13:11 +0800

    add cnyd contract

---
+++ cnyd/contracts/cnyd.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Cnyd is ERC20, ERC20Burnable, Pausable, Ownable {
+
+
+    uint8 private constant _decimals = 4;
+
+    constructor() ERC20("cnyd", "CNYD") {}
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
