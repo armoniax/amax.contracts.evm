f1386272360b919a9fb9795d4b130f632cfac493
SeanChen <aws4amazon@163.com>
2021-12-27 21:04:21 +0800

    Improve cnyd test

--- cnyd/test/cnyd-test.js
+++ cnyd/test/cnyd-test.js
@@ -6,25 +6,32 @@ const { ethers } = require("hardhat");
 // https://ethereum-waffle.readthedocs.io/en/latest/
 // https://github.com/EthWorks/Waffle
 
+const B = BigNumber.from;
+
 describe("Cnyd", function () {
   // global variants
+  let accounts;    
+  let owner;
+  let approvers;
+  let proposers;
+  let initApprovers;
 
   beforeEach(async () => {
     // init global variants
-  });
-
-  it("test Cnyd", async function () {
-    const Cnyd = await ethers.getContractFactory("Cnyd");
-    const accounts = await hre.ethers.getSigners();
-    expect(accounts).length.greaterThan(10);
-    const owner = accounts[0];
-    const approvers = accounts.slice(1, 4);
-    const proposers = accounts.slice(5, 8);
-    const initApprovers = [
+    accounts = await hre.ethers.getSigners();
+    owner = accounts[0];
+    approvers = accounts.slice(1, 4);
+    proposers = accounts.slice(5, 8);
+    initApprovers = [
       approvers[0].address,
       approvers[1].address,
       approvers[2].address,
     ];
+  });
+
+  it("test Cnyd", async function () {
+    const Cnyd = await ethers.getContractFactory("Cnyd");
+    expect(accounts).length.greaterThan(10);
 
     console.log("deploy contract. approvers:", initApprovers);
     const cnyd = await Cnyd.connect(owner).deploy(initApprovers);
@@ -47,10 +54,15 @@ describe("Cnyd", function () {
     expect(mintAmount).equal(100000000);
 
     console.log("approve mint. amount:", 10000_0000);
-    await cnyd
-      .connect(approvers[0])
-      .approveMint(proposers[0].address, true, 10000_0000);
+    await cnyd.connect(approvers[0]).approveMint(proposers[0].address, true, 10000_0000);
+    await cnyd.connect(approvers[1]).approveMint(proposers[0].address, true, 10000_0000);
     const [,, retApprovers1] = await cnyd.getMintProposal(proposers[0].address);
-    expect(retApprovers1[0]).to.equal(approvers[0].address);
+    expect(retApprovers1).to.deep.equal([approvers[0].address, approvers[1].address]);
+
+    await cnyd.connect(approvers[2]).approveMint(proposers[0].address, true, 10000_0000);
+    expect(await cnyd.getMintProposal(proposers[0].address)).to.deep.equal([B(0), B(0), []]);
+    expect(await cnyd.totalSupply()).to.equal(10000_0000);
+    expect(await cnyd.balanceOf(owner.address)).to.equal(10000_0000);
+
   });
 });
