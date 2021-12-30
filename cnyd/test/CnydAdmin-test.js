247b37835e4cba182813858886ef70833ba987e4
SeanChen <aws4amazon@163.com>
2021-12-30 18:10:34 +0800

    add burn proposal test

--- cnyd/test/CnydAdmin-test.js
+++ cnyd/test/CnydAdmin-test.js
@@ -58,7 +58,7 @@ describe("CnydAdmin", function () {
     expect(await cnydToken.admin()).equal(cnydAdmin.address);
   });
 
-  it("test proposeMint", async function () {
+  it("test Mint and Burn Proposal", async function () {
 
     await cnydAdmin.connect(owner).setProposer(proposers[0].address, true);
     expect(await cnydAdmin.proposers(proposers[0].address)).equal(true);
@@ -67,7 +67,7 @@ describe("CnydAdmin", function () {
     expect(proposeMintReceipt.events.length).to.equal(1);
     expect(proposeMintReceipt.events[0].event).to.equal('MintProposed');
 
-    const blockTime = B((await ethers.provider.getBlock(proposeMintReceipt.blockNumber)).timestamp);
+    let blockTime = B((await ethers.provider.getBlock(proposeMintReceipt.blockNumber)).timestamp);
 
     expect(proposeMintReceipt.events[0].args).to.deep.equal([proposers[0].address, users[0].address, B(10000_000000)]);
     expect(await cnydAdmin.getMintProposal(proposers[0].address)).to.deep.equal([users[0].address, B(10000_000000), blockTime, []]);
@@ -75,12 +75,38 @@ describe("CnydAdmin", function () {
     await cnydAdmin.connect(approvers[1]).approveMint(proposers[0].address, true, users[0].address, 10000_000000);
     expect((await cnydAdmin.getMintProposal(proposers[0].address))[3]).to.deep.equal([approvers[0].address, approvers[1].address]);
 
-    const approveReceipt = await (await cnydAdmin.connect(approvers[2]).approveMint(
+    const mintApproveReceipt = await (await cnydAdmin.connect(approvers[2]).approveMint(
         proposers[0].address, true, users[0].address, 10000_000000)).wait();
 
-    expect(approveReceipt.events[0].event).to.equal('MintApproved');
+    expect(mintApproveReceipt.events[0].event).to.equal('MintApproved');
     expect(await cnydToken.balanceOf(users[0].address)).equal(10000_000000);
     expect(await cnydToken.totalSupply()).equal(10000_000000);
 
+    // transfer burned amount to cnyd contract
+    await cnydToken.connect(users[0]).transfer(cnydToken.address, 1000_000000)
+    expect(await cnydToken.balanceOf(users[0].address)).equal(9000_000000);
+    expect(await cnydToken.balanceOf(cnydToken.address)).equal(1000_000000);
+
+    // proposeBurn
+    const proposeBurnReceipt = await (await cnydAdmin.connect(proposers[0]).proposeBurn(100_000000)).wait(1);
+    expect(proposeBurnReceipt.events.length).to.equal(1);
+    expect(proposeBurnReceipt.events[0].event).to.equal('BurnProposed');
+    expect(proposeBurnReceipt.events[0].args).to.deep.equal([proposers[0].address, B(100_000000)]);
+
+    blockTime = B((await ethers.provider.getBlock(proposeBurnReceipt.blockNumber)).timestamp);
+
+    expect(await cnydAdmin.getBurnProposal(proposers[0].address)).to.deep.equal([B(100_000000), blockTime, []]);
+    await cnydAdmin.connect(approvers[0]).approveBurn(proposers[0].address, true, 100_000000);
+    await cnydAdmin.connect(approvers[1]).approveBurn(proposers[0].address, true, 100_000000);
+    expect((await cnydAdmin.getBurnProposal(proposers[0].address))[2]).to.deep.equal([approvers[0].address, approvers[1].address]);
+
+    const burnApproveReceipt = await (await cnydAdmin.connect(approvers[2]).approveBurn(
+        proposers[0].address, true, 100_000000)).wait();
+
+    expect(burnApproveReceipt.events[0].event).to.equal('BurnApproved');
+    expect(await cnydToken.balanceOf(cnydToken.address)).equal(900_000000);
+    expect(await cnydToken.totalSupply()).equal(9900_000000);
+
   });
+
 });
