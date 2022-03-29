59e4c4f6ac735d7b658df86fbce11d6e37d6749a
masteray <masteraychen@gmail.com>
2022-03-29 17:06:26 +0800

    add amax erc20/bep20

---
+++ amaxBEP20/contracts/AmaxBEP20.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+abstract contract Ownable {
+    event OwnershipProposed(address indexed newOwner);
+    event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+
+    address private _owner;
+    address private _proposedOwner;
+
+    /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function owner() public view returns(address) {
+        return _owner;
+    }
+
+    function proposedOwner() public view returns(address) {
+        return _proposedOwner;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the proposed owner.
+    */
+    modifier onlyProposedOwner() {
+        require(_proposedOwner != address(0) && msg.sender == _proposedOwner, 
+            "Ownable: caller is not the proposed owner");
+        _;
+    }
+
+    modifier onlyNonZeroAccount(address account) {
+        require(account != address(0), "zero account not allowed" );
+        _;
+    }
+
+    /**
+    * @dev propose a new owner by an existing owner
+    * @param newOwner The address proposed to transfer ownership to.
+    */
+    function proposeOwner(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _proposedOwner = newOwner;
+        emit OwnershipProposed(newOwner);
+    }
+
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    */
+    function takeOwnership() public onlyProposedOwner {
+        _transferOwnership(_proposedOwner);
+        _proposedOwner = address(0);
+    }
+
+    /**
+    * @dev Transfers control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: zero address not allowed");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+}
+
+contract AmaxBEP20 is ERC20, Pausable, Ownable {
+
+    uint8 private constant _decimals = 8;
+
+    constructor() ERC20("Armonia Meta Chain Base Coin", "AMAX") {
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `to`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+
+    /**
+     * @dev Destroys `amount` tokens from contract account, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function burn(uint256 amount) public onlyOwner {
+        _burn(address(this), amount);
+    }
+
+   function _transfer(address sender, address recipient, uint256 amount) internal override whenNotPaused {
+        super._transfer(sender, recipient, amount);
+    }
+
+}
---
+++ amaxERC20/test/CnydBEP20-test.js
@@ -0,0 +1,51 @@
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+
+// https://getwaffle.io/
+// https://ethereum-waffle.readthedocs.io/en/latest/
+// https://github.com/EthWorks/Waffle
+
+const B = BigNumber.from;
+
+describe("CnydBEP20", function () {
+  // global variants
+  let accounts;    
+  let owner;
+  let users;
+
+  beforeEach(async () => {
+    // init global variants
+    accounts = await hre.ethers.getSigners();    
+    expect(accounts).length.greaterThan(10);
+    owner = accounts[0];
+    users = accounts.slice(1, 3);
+  });
+
+  it("test CnydBEP20", async function () {
+    const Cnyd = await ethers.getContractFactory("CnydBEP20");
+
+    const cnyd = await Cnyd.connect(owner).deploy();
+    await cnyd.deployed();
+    expect(await cnyd.owner()).equal(owner.address);
+
+    // mint
+    await cnyd.connect(owner).mint(users[0].address, 10000_000000);
+    expect(await cnyd.balanceOf(users[0].address)).equal(10000_000000);
+    expect(await cnyd.totalSupply()).equal(10000_000000);    
+
+    // transfer
+    await cnyd.connect(users[0]).transfer(users[1].address, 100_000000);
+    expect(await cnyd.balanceOf(users[0].address)).equal(9900_000000);
+    expect(await cnyd.balanceOf(users[1].address)).equal(100_000000);
+    expect(await cnyd.totalSupply()).equal(10000_000000);  
+
+
+    // burn
+    await cnyd.connect(users[0]).transfer(cnyd.address, 100_000000);
+    expect(await cnyd.balanceOf(cnyd.address)).equal(100_000000);
+    await cnyd.connect(owner).burn(100_000000);
+    expect(await cnyd.balanceOf(cnyd.address)).equal(0);
+    expect(await cnyd.totalSupply()).equal(10000_000000 - 100_000000);  
+  });
+});
